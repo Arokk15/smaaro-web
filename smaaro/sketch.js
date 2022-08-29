7da9d031eb9b30e0fc8ae9bf53bb4ba45fce0401
Arokk15 <99024377+Arokk15@users.noreply.github.com>
2022-08-29 17:53:30 +0530

    Add files via upload

---
+++ smaaro/sketch.js
@@ -0,0 +1,6 @@
+var b1=document.getElementById('box1')
+var b2=document.getElementById('box2')
+
+b2.addEventListener('click',function (event){
+b1.style.cssText="backgroung-color:red;width:100%;height:300px;border:solid;transform: translateX(0px)" 
+})
